ee87ffe6702c9b36d61c5a79d0ebb3e51fb9644b
yuanyc <848555155@qq.com>
2021-09-06 00:00:18 +0800

    ✨ feat(YYC.Module.Admin.Domain): 允许可空

--- Modules/YYC.Module.Admin/YYC.Module.Admin.Domain/Accounts/Account.cs
+++ Modules/YYC.Module.Admin/YYC.Module.Admin.Domain/Accounts/Account.cs
@@ -1,11 +1,35 @@
+
+
 namespace YYC.Module.Admin.Accounts;
 
 [Comment("账户表")]
 public class Account
 {
+    public Account(
+                string name,
+                string userName,
+                string password)
+    {
+        Name = name;
+        UserName = userName;
+        Password = password;
+    }
+
     [Comment("账户主键")]
     public Guid AccountId { get; set; }
 
     [Comment("姓名")]
+    [Required]
     public string Name { get; set; }
+
+    [Comment("用户名")]
+    [Required]
+    public string UserName { get; set; }
+
+    [Comment("密码")]
+    [Required]
+    public string Password { get; set; }
+
+    [Comment("虚拟删除标记")]
+    public bool Deleted { get; set; } = false;
 }
--- Modules/YYC.Module.Admin/YYC.Module.Admin.Domain/AdminDbContext.cs
+++ Modules/YYC.Module.Admin/YYC.Module.Admin.Domain/AdminDbContext.cs
@@ -1,4 +1,5 @@
 global using Microsoft.EntityFrameworkCore;
+global using System.ComponentModel.DataAnnotations;
 using YYC.Module.Admin.Accounts;
 namespace YYC.Module.Admin;
 public class AdminDbContext : DbContext
@@ -6,6 +7,14 @@ public class AdminDbContext : DbContext
     public AdminDbContext(DbContextOptions<AdminDbContext> options)
         : base(options)
     {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Account>()
+            .Property(b => b.Name)
+            .IsRequired();
     }
 
     public DbSet<Account> Accounts { get; set; }
