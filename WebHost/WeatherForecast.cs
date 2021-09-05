a4eeba206fd7288b2f9b807abf5e39e0c2bc3d41
yuanyc <848555155@qq.com>
2021-09-05 15:50:50 +0800

    init

--- WebHost/WeatherForecast.cs
+++ WebHost/WeatherForecast.cs
@@ -1,15 +1,11 @@
-﻿using System;
-
-namespace WebHost
+﻿namespace WebHost;
+public class WeatherForecast
 {
-    public class WeatherForecast
-    {
-        public DateTime Date { get; set; }
+    public DateTime Date { get; set; }
 
-        public int TemperatureC { get; set; }
+    public int TemperatureC { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string Summary { get; set; }
-    }
+    public string Summary { get; set; }
 }
